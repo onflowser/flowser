fdf7f43b64fced8df83a586c348ebe91c8d05f51
jernej.gololicic <jernej.gololicic@gmail.com>
2021-09-28 15:46:44 +0200

    - Add TSC to pre-commit hook to prevent committing TS errors

--- frontend/src/pages/accounts/Accounts.tsx
+++ frontend/src/pages/accounts/Accounts.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useSearch } from '../../shared/hooks/search';
 
 const Accounts = () => {
-    const { term, setPlaceholder } = useSearch();
+    const { searchTerm, setPlaceholder } = useSearch();
 
     useEffect(() => {
         setPlaceholder('Search accounts');
@@ -11,7 +11,7 @@ const Accounts = () => {
     return (
         <div>
             <h2>Accounts</h2>
-            <span>Search value: {term}</span>
+            <span>Search value: {searchTerm}</span>
         </div>
     );
 };
