67068e0414e5cba5ad8756730db3a3a69430fdae
jernej.gololicic <jernej.gololicic@gmail.com>
2021-09-30 14:19:36 +0200

    - Better naming for props parameter

--- frontend/src/shared/hooks/filter-data.ts
+++ frontend/src/shared/hooks/filter-data.ts
@@ -2,12 +2,12 @@ export interface UseFilterDataHook<T> {
     filteredData: T;
 }
 
-export const useFilterData = <T>(data: T[], search: string, props?: string[]): UseFilterDataHook<T[]> => {
+export const useFilterData = <T>(data: T[], search: string, filterByProps?: string[]): UseFilterDataHook<T[]> => {
     if (!Array.isArray(data) || data.length === 0) {
         return { filteredData: data };
     }
 
-    props = props || Object.keys(data[0]);
-    const filteredData = data.filter((item) => JSON.stringify(item, props).indexOf(search.toString()) !== -1);
+    filterByProps = filterByProps || Object.keys(data[0]);
+    const filteredData = data.filter((item) => JSON.stringify(item, filterByProps).indexOf(search.toString()) !== -1);
     return { filteredData };
 };
