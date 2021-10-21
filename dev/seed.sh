8014516b11fd2285a74fcdcc202be81c3c68dac6
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-10-21 23:39:37 +0200

    removed old helpers

--- dev/seed.sh
+++
@@ -1,8 +0,0 @@
-flow accounts add-contract HelloWorld contracts/HelloWorld.cdc
-
-count="${1:-4}" # defaults to 4 transactions
-
-for ((n=0;n<count;n++))
-do
-  flow transactions send transactions/HelloWorld.cdc HelloWorld
-done
