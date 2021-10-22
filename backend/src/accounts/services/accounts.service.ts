681b643c67a8a727e939d2331905cff4abebe5bb
jernej.gololicic <jernej.gololicic@gmail.com>
2021-10-22 19:11:10 +0200

    - Add txCount to account polling

--- backend/src/accounts/services/accounts.service.ts
+++ backend/src/accounts/services/accounts.service.ts
@@ -9,52 +9,85 @@ import { MongoRepository } from "typeorm";
 export class AccountsService {
 
   constructor (
-    @InjectRepository(Account)
-    private accountRepository: MongoRepository<Account>
+        @InjectRepository(Account)
+        private accountRepository: MongoRepository<Account>
   ) {}
 
-  create(createAccountDto: CreateAccountDto) {
-    return this.accountRepository.save(createAccountDto);
-  }
+    create(createAccountDto: CreateAccountDto) {
+        return this.accountRepository.save(createAccountDto);
+    }
 
-  findAll() {
-    return this.accountRepository.find();
-  }
+    findAll() {
+        return this.accountRepository.find();
+    }
 
-  findAllNewerThanTimestamp(timestamp): Promise<Account[]> {
-    return this.accountRepository.find({
-      where: {createdAt: {$gt: timestamp}}
-    });
-  }
+    findAllNewerThanTimestamp(timestamp): Promise<Account[]> {
+        return this.accountRepository.aggregate([
+            {$match: {createdAt: {$gt: timestamp}}},
+            {
+                $lookup: {
+                    from: "transactions",
+                    let: {address: "$address"},
+                    pipeline: [
+                        {
+                            $match:
+                                {
+                                    $expr: {
+                                        $eq: [{$concat: ["0x", "$payer"]}, "$$address"]
+                                    }
+                                }
+                        },
+                        {$count: "count"},
+                    ],
+                    as: "txCount"
+                }
+            },
+            {$unwind: "$txCount"},
+            {
+                $project: {
+                    createdAt: 1,
+                    updatedAt: 1,
+                    address: 1,
+                    balance: 1,
+                    code: 1,
+                    keys: 1,
+                    contracts: 1,
+                    "txCount": "$txCount.count"
+                }
+            }
+    ]).toArray()
 
-  async findOne(id: string) {
+
+    }
+
+    async findOne(id: string) {
     const [account] = await this.accountRepository.find({ where: {id} });
-    if (account) {
-      return account;
-    } else {
-      throw new NotFoundException("Account not found")
+        if (account) {
+            return account;
+        } else {
+            throw new NotFoundException("Account not found")
+        }
     }
-  }
 
-  async findOneByAddress(address: string) {
+    async findOneByAddress(address: string) {
     const account = await this.accountRepository.findOne({ where: { address }});
-    if (account) {
-      return account;
-    } else {
-      throw new NotFoundException("Account not found")
+        if (account) {
+            return account;
+        } else {
+            throw new NotFoundException("Account not found")
+        }
     }
-  }
 
-  update(address: string, updateAccountDto: UpdateAccountDto) {
-    // we refetch and insert the whole account entity
-    // contracts & keys can be added or removed
-    // therefore collection needs to be replaced and not just updated
-    return this.accountRepository.replaceOne(
+    update(address: string, updateAccountDto: UpdateAccountDto) {
+        // we refetch and insert the whole account entity
+        // contracts & keys can be added or removed
+        // therefore collection needs to be replaced and not just updated
+        return this.accountRepository.replaceOne(
       { address },
-      updateAccountDto,
-      // TODO: why default emulator-account creation event is not logged inside a transaction ?
-      // this is why we need to create new account if account doesn't exists (edge case)
+            updateAccountDto,
+            // TODO: why default emulator-account creation event is not logged inside a transaction ?
+            // this is why we need to create new account if account doesn't exists (edge case)
       { upsert: true }
-    );
-  }
+        );
+    }
 }
