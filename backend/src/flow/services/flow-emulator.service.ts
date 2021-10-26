a70283d6930833a021c5d7d865633adaa421cfe8
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-10-26 23:34:36 +0200

    - rearranged methods in emulator service

--- backend/src/flow/services/flow-emulator.service.ts
+++ backend/src/flow/services/flow-emulator.service.ts
@@ -32,85 +32,9 @@ export class FlowEmulatorService {
         this.flowCliConfig.configure(this.projectId, this.configuration);
     }
 
-    static parseLogLine (line: string) {
-        const keyValuePairs = [];
-        // https://regex101.com/r/gVlMZ0/1
-        // tokenizes log lines into key=value pair array
-        const matches = line.matchAll(/[a-z]+=("([^"]+"))|([^\s]+)/g);
-        for (let [match] of matches) {
-            // each match is of form key=value or key="foo bar"
-            const [key, value] = match
-                .toString()
-                .replace(/"/g, "") // remove " chars if they exist
-                .split("="); // split into [key, value] pairs
-            keyValuePairs.push({ [key]: value })
-        }
-        return keyValuePairs.reduce((p, c) => ({ ...p, ...c }), {})
-    }
-
-    public getError () {
-        for (let i = this.logs.length - 1; i > 0; i--) {
-            if (this.logs[i].includes("level=error")) {
-                const errorLine = FlowEmulatorService.parseLogLine(this.logs[i]);
-                return errorLine.error ? new Error(errorLine.error) : null
-            }
-        }
-        return null;
-    }
-
-    private setState (state: FlowEmulatorState) {
-        console.log("[Flowser] emulator state changed: ", state)
-        this.events.emit(state);
-        this.state = state;
-    }
-
-    private isState (state: FlowEmulatorState) {
-        return this.state === state;
-    }
-
     async init () {
         console.log(`[Flowser] initialising emulator for project: ${this.projectId}`)
         await this.flowCliConfig.init();
-    }
-
-    private static flag (name: string, userValue: any, defaultValue?: any) {
-        const value = userValue || defaultValue;
-        return value ? `--${name}=${value}` : undefined;
-    }
-
-    private getFlags () {
-        const { flag } = FlowEmulatorService;
-
-        // https://github.com/onflow/flow-emulator#configuration
-        return [
-            flag("port", this.configuration.rpcServerPort),
-            flag("http-port", this.configuration.httpServerPort),
-            flag("block-time", this.configuration.blockTime),
-            flag("service-priv-key", this.configuration.servicePrivateKey),
-            flag("service-pub-key", this.configuration.servicePublicKey),
-            flag("dbpath", this.configuration.databasePath),
-            flag("token-supply", this.configuration.tokenSupply),
-            flag("transaction-expiry", this.configuration.transactionExpiry),
-            flag("storage-per-flow", this.configuration.storagePerFlow),
-            flag("min-account-balance", this.configuration.minAccountBalance),
-            flag("transaction-max-gas-limit", this.configuration.transactionMaxGasLimit),
-            flag("script-gas-limit", this.configuration.scriptGasLimit),
-            flag("service-sig-algo", this.configuration.serviceSignatureAlgorithm),
-            flag("service-hash-algo", this.configuration.serviceHashAlgorithm),
-            flag("storage-limit", this.configuration.storageLimit),
-            flag("transaction-fees", this.configuration.transactionFees),
-            flag("dbpath", this.configuration.databasePath || this.flowCliConfig.databaseDirPath),
-            flag("persist", this.configuration.persist),
-            flag("verbose", this.configuration.verboseLogging),
-            flag("init", true)
-        ].filter(Boolean);
-    }
-
-    isRunning () {
-        return (
-            this.emulatorProcess &&
-            [FlowEmulatorState.STARTED, FlowEmulatorState.RUNNING].includes(this.state)
-        );
     }
 
     async start (cb: StartCallback = () => null) {
@@ -177,6 +101,23 @@ export class FlowEmulatorService {
         }))
     }
 
+    stop () {
+        return new Promise(resolve => {
+            console.log(`[Flowser] stopping emulator: ${this.isRunning()}`)
+            if (this.isRunning()) {
+                console.log(`[Flowser] stopping emulator process: ${this.emulatorProcess.pid}`)
+                const isKilled = this.emulatorProcess.kill();
+                // resolve only when the emulator process exits
+                this.events.on(FlowEmulatorState.STOPPED, () => {
+                    console.log(`[Flowser] stopped emulator process: ${this.emulatorProcess.pid}`);
+                    resolve(isKilled)
+                })
+            } else {
+                resolve(true);
+            }
+        })
+    }
+
     // called when emulator is up and running
     async onServerRunning () {
         this.setState(FlowEmulatorState.RUNNING)
@@ -215,21 +156,59 @@ export class FlowEmulatorService {
         return { address, publicKey, privateKey }
     }
 
-    stop () {
-        return new Promise(resolve => {
-            console.log(`[Flowser] stopping emulator: ${this.isRunning()}`)
-            if (this.isRunning()) {
-                console.log(`[Flowser] stopping emulator process: ${this.emulatorProcess.pid}`)
-                const isKilled = this.emulatorProcess.kill();
-                // resolve only when the emulator process exits
-                this.events.on(FlowEmulatorState.STOPPED, () => {
-                    console.log(`[Flowser] stopped emulator process: ${this.emulatorProcess.pid}`);
-                    resolve(isKilled)
-                })
-            } else {
-                resolve(true);
+    isRunning () {
+        return (
+            this.emulatorProcess &&
+            [FlowEmulatorState.STARTED, FlowEmulatorState.RUNNING].includes(this.state)
+        );
+    }
+
+    public getError () {
+        for (let i = this.logs.length - 1; i > 0; i--) {
+            if (this.logs[i].includes("level=error")) {
+                const errorLine = FlowEmulatorService.parseLogLine(this.logs[i]);
+                return errorLine.error ? new Error(errorLine.error) : null
             }
-        })
+        }
+        return null;
+    }
+
+    private setState (state: FlowEmulatorState) {
+        console.log("[Flowser] emulator state changed: ", state)
+        this.events.emit(state);
+        this.state = state;
+    }
+
+    private isState (state: FlowEmulatorState) {
+        return this.state === state;
+    }
+
+    private getFlags () {
+        const { flag } = FlowEmulatorService;
+
+        // https://github.com/onflow/flow-emulator#configuration
+        return [
+            flag("port", this.configuration.rpcServerPort),
+            flag("http-port", this.configuration.httpServerPort),
+            flag("block-time", this.configuration.blockTime),
+            flag("service-priv-key", this.configuration.servicePrivateKey),
+            flag("service-pub-key", this.configuration.servicePublicKey),
+            flag("dbpath", this.configuration.databasePath),
+            flag("token-supply", this.configuration.tokenSupply),
+            flag("transaction-expiry", this.configuration.transactionExpiry),
+            flag("storage-per-flow", this.configuration.storagePerFlow),
+            flag("min-account-balance", this.configuration.minAccountBalance),
+            flag("transaction-max-gas-limit", this.configuration.transactionMaxGasLimit),
+            flag("script-gas-limit", this.configuration.scriptGasLimit),
+            flag("service-sig-algo", this.configuration.serviceSignatureAlgorithm),
+            flag("service-hash-algo", this.configuration.serviceHashAlgorithm),
+            flag("storage-limit", this.configuration.storageLimit),
+            flag("transaction-fees", this.configuration.transactionFees),
+            flag("dbpath", this.configuration.databasePath || this.flowCliConfig.databaseDirPath),
+            flag("persist", this.configuration.persist),
+            flag("verbose", this.configuration.verboseLogging),
+            flag("init", true)
+        ].filter(Boolean);
     }
 
     private execute (bin = "", args, parsedOutput = true): Promise<string | string[][]> {
@@ -269,6 +248,27 @@ export class FlowEmulatorService {
                 return value;
             }
         }
+    }
+
+    static parseLogLine (line: string) {
+        const keyValuePairs = [];
+        // https://regex101.com/r/gVlMZ0/1
+        // tokenizes log lines into key=value pair array
+        const matches = line.matchAll(/[a-z]+=("([^"]+"))|([^\s]+)/g);
+        for (let [match] of matches) {
+            // each match is of form key=value or key="foo bar"
+            const [key, value] = match
+                .toString()
+                .replace(/"/g, "") // remove " chars if they exist
+                .split("="); // split into [key, value] pairs
+            keyValuePairs.push({ [key]: value })
+        }
+        return keyValuePairs.reduce((p, c) => ({ ...p, ...c }), {})
+    }
+
+    private static flag (name: string, userValue: any, defaultValue?: any) {
+        const value = userValue || defaultValue;
+        return value ? `--${name}=${value}` : undefined;
     }
 
 }
