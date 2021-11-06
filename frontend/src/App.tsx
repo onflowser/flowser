d64bf09ab86c0723e30cdab0026e499236c5bce0
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-11-06 18:15:20 +0100

    - remove lazy loading components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,21 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Redirect, Route, Switch, withRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Layout from './core/components/layout/Layout';
-import Start from './pages/start/Start';
 import { routes } from './shared/constants/routes';
 import { UiStateContextProvider } from './shared/contexts/ui-state.context';
 import { useSearch } from './shared/hooks/search';
 import './App.scss';
 import { toastOptions } from './shared/config/toast';
 
-const LazyAccounts = React.lazy(() => import('./pages/accounts/Accounts'));
-const LazyBlocks = React.lazy(() => import('./pages/blocks/Blocks'));
-const LazyTransactions = React.lazy(() => import('./pages/transactions/Transactions'));
-const LazyContracts = React.lazy(() => import('./pages/contracts/Contracts'));
-const LazyEvents = React.lazy(() => import('./pages/events/Events'));
-const LazyLogs = React.lazy(() => import('./pages/logs/Logs'));
+// pages
+import Start from './pages/start/Start';
+import Accounts from './pages/accounts/Accounts';
+import Blocks from './pages/blocks/Blocks';
+import Transactions from './pages/transactions/Transactions';
+import Contracts from './pages/contracts/Contracts';
+import Events from './pages/events/Events';
+import Logs from './pages/logs/Logs';
 
 const RouteWithLayout = (props: any) => (
     <Layout>
@@ -41,25 +42,23 @@ const BrowserRouterEvents = withRouter(({ children, history, location }) => {
 
 export const App = () => {
     return (
-        <Suspense fallback="loading ...">
-            <UiStateContextProvider>
-                <BrowserRouter>
-                    <BrowserRouterEvents>
-                        <Switch>
-                            <Route path={`/${routes.start}`} component={Start} />
-                            <RouteWithLayout path={`/${routes.accounts}`} component={LazyAccounts} />
-                            <RouteWithLayout path={`/${routes.blocks}`} component={LazyBlocks} />
-                            <RouteWithLayout path={`/${routes.transactions}`} component={LazyTransactions} />
-                            <RouteWithLayout path={`/${routes.contracts}`} component={LazyContracts} />
-                            <RouteWithLayout path={`/${routes.events}`} component={LazyEvents} />
-                            <RouteWithLayout path={`/${routes.logs}`} component={LazyLogs} />
-                            <Redirect from="*" to={`/${routes.start}`} />
-                        </Switch>
-                        <Toaster position="bottom-center" gutter={8} toastOptions={toastOptions} />
-                    </BrowserRouterEvents>
-                </BrowserRouter>
-            </UiStateContextProvider>
-        </Suspense>
+        <UiStateContextProvider>
+            <BrowserRouter>
+                <BrowserRouterEvents>
+                    <Switch>
+                        <Route path={`/${routes.start}`} component={Start} />
+                        <RouteWithLayout path={`/${routes.accounts}`} component={Accounts} />
+                        <RouteWithLayout path={`/${routes.blocks}`} component={Blocks} />
+                        <RouteWithLayout path={`/${routes.transactions}`} component={Transactions} />
+                        <RouteWithLayout path={`/${routes.contracts}`} component={Contracts} />
+                        <RouteWithLayout path={`/${routes.events}`} component={Events} />
+                        <RouteWithLayout path={`/${routes.logs}`} component={Logs} />
+                        <Redirect from="*" to={`/${routes.start}`} />
+                    </Switch>
+                    <Toaster position="bottom-center" gutter={8} toastOptions={toastOptions} />
+                </BrowserRouterEvents>
+            </BrowserRouter>
+        </UiStateContextProvider>
     );
 };
 
