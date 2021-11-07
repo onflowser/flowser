f12e06ab18fc2d69262ab0dec3c082a672e6cf09
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-11-07 17:16:36 +0100

    - disable prod restarts for frontend container - additional nginx proxy config - disable letsencrypt staging mode

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -12,7 +12,7 @@ domains=(app.flowser.dev www.app.flowser.dev)
 rsa_key_size=4096
 data_path="./server/certbot"
 email="bartolomej.kozorog@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
