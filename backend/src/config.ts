d44e6844c396531af78e9229693a966313f34860
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-11-07 21:55:49 +0100

    enable local emulator usage with hosted version

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,5 +4,5 @@ export default {
   dataFetchInterval: parseInt(process.env.DATA_FETCH_INTERVAl) || 3000,
   // __dirname is <project-root>/dist folder
   flowserRootDir: join(__dirname, '..', '.flowser'),
-  userManagedEmulatorPort: parseInt(process.env.USER_MANAGED_EMULATOR_PORT) || 8081
+  userManagedEmulatorPort: parseInt(process.env.USER_MANAGED_EMULATOR_PORT)
 }
