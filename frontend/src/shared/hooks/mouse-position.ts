9c960f0a88581f389f05605fdc652789abc581bd
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2022-03-03 11:49:57 +0100

    feat: control drawer size with mouse position

---
+++ frontend/src/shared/hooks/mouse-position.ts
@@ -0,0 +1,19 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export const useMouseMove = (track: boolean): MouseEvent | undefined => {
+    const [event, setEvent] = useState<MouseEvent>();
+
+    const onMouseMove = useCallback((e) => {
+        setEvent(e);
+    }, []);
+
+    useEffect(() => {
+        if (track) {
+            document.addEventListener('mousemove', onMouseMove);
+        } else {
+            document.removeEventListener('mousemove', onMouseMove);
+        }
+    }, [track]);
+
+    return event;
+};
