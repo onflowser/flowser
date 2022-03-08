d94d26fe458954540c633fd7afe99daa6786298e
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2022-03-08 21:43:43 +0100

    fix: storage-data.service refactor

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,5 +4,6 @@ export default {
   dataFetchInterval: parseInt(process.env.DATA_FETCH_INTERVAl) || 3000,
   // __dirname is <project-root>/dist folder
   flowserRootDir: join(__dirname, '..', '.flowser'),
-  userManagedEmulatorPort: parseInt(process.env.USER_MANAGED_EMULATOR_PORT)
+  userManagedEmulatorPort: parseInt(process.env.USER_MANAGED_EMULATOR_PORT) || 8081,
+  storageServerPort: parseInt(process.env.FLOW_STORAGE_SERVER_PORT) || 8889
 }
