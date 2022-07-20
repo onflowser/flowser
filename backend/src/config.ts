bd65c5f449e8e72d8c4e79eb6aa079e7fedac962
Bart <bartolomej.kozorog@gmail.com>
2022-07-20 13:39:10 +0200

    move flow env variables to config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,6 +2,11 @@ import { join } from "path";
 import { cleanEnv, str, num } from "envalid";
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
+const defaultServiceAccountPubKey =
+  "5a6a7bdb81838e40fc615d4c0eed3d4caacfc7f47a89d319caa370aac6196113573738ba57e09ea5a27a192d48457ee5c0e32011bc10ef93383aabad24a9ce2a";
+const defaultServiceAccountPrivKey =
+  "680fa28962650ef346a7edf23d63967b0fcf44958488d0d48f8539ece6e92eba";
+
 export const env = cleanEnv(process.env, {
   DATABASE_TYPE: str({
     default: "mysql",
@@ -18,6 +23,15 @@ export const env = cleanEnv(process.env, {
   FLOW_STORAGE_SERVER_PORT: num({ default: 8889 }),
 
   HTTP_PORT: num({ default: 3001 }),
+
+  FLOW_EMULATOR_HTTP_PORT: num({ default: 8080 }),
+  FLOW_ACCOUNT_ADDRESS: str({ default: "f8d6e0586b0a20c7" }),
+  FLOW_ACCOUNT_PRIVATE_KEY: str({
+    default: defaultServiceAccountPrivKey,
+  }),
+  FLOW_ACCOUNT_PUBLIC_KEY: str({
+    default: defaultServiceAccountPubKey,
+  }),
 });
 
 export const databaseConfig: TypeOrmModuleOptions = {
