b3a56c59ac6578408bf9904cc4f6579735cabbba
MrakJakob <jm1349@student.uni-lj.si>
2022-09-01 13:17:24 +0200

    Added SideBar component

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useCallback } from "react";
+import React, { FunctionComponent, useCallback, useState } from "react";
 import classes from "./Layout.module.scss";
 import Navigation from "../navigation/Navigation";
 import Content from "../content/Content";
@@ -7,12 +7,18 @@ import Logs from "../../pages/logs/Logs";
 import { useLogDrawer } from "../../hooks/use-log-drawer";
 import { useNavigation } from "../../hooks/use-navigation";
 import { useGetCurrentProject } from "../../hooks/use-api";
+// import Button from "components/button/Button";
+import SideBar from "components/sidebar/SideBar";
 
 const Layout: FunctionComponent = ({ children }) => {
   const { logDrawerSize } = useLogDrawer();
   const { isSubNavigationVisible } = useNavigation();
   const { data } = useGetCurrentProject();
   const { project: currentProject } = data ?? {};
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsOpen((prevState) => !prevState);
+  };
   const getLogDrawerLayoutClass = useCallback(() => {
     return logDrawerSize === "tiny"
       ? ""
@@ -27,6 +33,8 @@ const Layout: FunctionComponent = ({ children }) => {
       {isSubNavigationVisible && (
         <SubNavigation className={classes.subNavigation} />
       )}
+      <SideBar toggled={isOpen} toggleSidebar={toggleSidebar} />
+      {/* <Button onClick={toggleSidebar}> TOGGLE SIDEBAR</Button> */}
       <Content className={classes.content}>{children} </Content>
       {!!currentProject?.emulator && (
         <Logs className={`${classes.logs} ${getLogDrawerLayoutClass()}`} />
---
+++ frontend/src/components/sidebar/SideBar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
+import { NavLink } from "react-router-dom";
+import classes from "./SideBar.module.scss";
+import { ReactComponent as SettingsIcon } from "../../assets/icons/settings-circle.svg";
+import { ReactComponent as ConnectIcon } from "../../assets/icons/connect-circle .svg";
+import { ReactComponent as SwitchIcon } from "../../assets/icons/switch.svg";
+import ColoredCircle from "components/colored-circle/ColoredCircle";
+
+export type Sidebar = {
+  toggled: boolean;
+  toggleSidebar: () => void;
+};
+
+function SideBar({ toggled, toggleSidebar }: Sidebar) {
+  return (
+    <Drawer
+      open={toggled}
+      direction={"right"}
+      onClose={toggleSidebar}
+      className={classes.sidebar}
+      size={300}
+    >
+      <div className={classes.menu}>
+        <div>
+          <NavLink to={"#"} className={classes.menuItem}>
+            <SwitchIcon className={classes.icon} />
+            <div className={classes.text}>Switch project</div>
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to={"#"} className={classes.menuItem}>
+            <ConnectIcon className={classes.icon} />
+            <div className={classes.text}>Connect</div>
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to={"#"} className={classes.menuItem}>
+            <SettingsIcon className={classes.icon} />
+            <div className={classes.text}>Settings</div>
+          </NavLink>
+        </div>
+      </div>
+    </Drawer>
+  );
+}
+
+export default SideBar;
