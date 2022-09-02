0d54e1eaafb11119ccc6a40d614cab9f2f33e55f
Bart <bartolomej.kozorog@gmail.com>
2022-09-02 04:20:53 +0200

    refactor start/main component to make it more readable

--- frontend/src/pages/start/main/Main.tsx
+++ frontend/src/pages/start/main/Main.tsx
@@ -1,4 +1,9 @@
-import React, { FunctionComponent, useCallback, useEffect } from "react";
+import React, {
+  FunctionComponent,
+  ReactElement,
+  useCallback,
+  useEffect,
+} from "react";
 import { Link, RouteChildrenProps, useHistory } from "react-router-dom";
 import { routes } from "../../../constants/routes";
 import IconButton from "../../../components/icon-button/IconButton";
@@ -21,32 +26,36 @@ import { useConfirmDialog } from "../../../contexts/confirm-dialog.context";
 import { useProjectActions } from "../../../contexts/project-actions.context";
 import { SimpleButton } from "../../../components/simple-button/SimpleButton";
 
-type IProps = RouteChildrenProps;
+type ProjectTab = {
+  id: string;
+  label: string;
+  isDefault?: boolean;
+  content: ReactElement;
+};
 
-const Main: FunctionComponent<IProps> = (props) => {
+const tabs: ProjectTab[] = [
+  {
+    id: "projects",
+    label: "Projects",
+    isDefault: true,
+    content: <ProjectsListContent />,
+  },
+  {
+    id: "about",
+    label: "About",
+    content: <AboutContent />,
+  },
+];
+
+const Main: FunctionComponent<RouteChildrenProps> = (props) => {
   const { showDialog } = useConfirmDialog();
-  const { removeProject } = useProjectActions();
-  const { searchTerm, setPlaceholder } = useSearch("projectSearch");
-
-  useEffect(() => {
-    setPlaceholder("Search projects");
-  }, []);
-
-  const tab = props.location.hash?.replace("#", "");
   const history = useHistory();
-  const projectService = ProjectsService.getInstance();
-  const { projects } = useGetAllProjects()?.data ?? {};
 
-  const onQuickstart = async (project: Project) => {
-    try {
-      await projectService.useProject(project.id);
-      splitbee.track(`Start: use ${project}`);
-      history.push(`/${routes.firstRouteAfterStart}`);
-    } catch (e: unknown) {
-      console.error("Can't open project:", e);
-      toast.error("Can't open project");
-    }
-  };
+  const providedTabId = props.location.hash?.replace("#", "");
+  const providedTab = tabs.find((tab) => tab.id === providedTabId);
+  const defaultTab = tabs.find((tab) => tab.isDefault);
+  const fallbackTab = tabs[0];
+  const activeTab = providedTab ?? defaultTab ?? fallbackTab;
 
   const onConfigure = useCallback(() => {
     history.push(`/${routes.start}/configure`);
@@ -65,84 +74,6 @@ const Main: FunctionComponent<IProps> = (props) => {
     });
   }
 
-  const tabs = [
-    {
-      id: "projects",
-      label: "Projects",
-      default: true,
-      content:
-        projects && projects.length ? (
-          <div className={classes.projectList}>
-            <div className={classes.projectListHeader}>
-              <Search
-                className={classes.projectSearch}
-                context="projectSearch"
-              />
-            </div>
-            <ul className={classes.projectListBody}>
-              {projects
-                ?.filter((p) => !searchTerm || p.name.includes(searchTerm))
-                .map((project) => (
-                  <li key={project.id} className={classes.projectWrapper}>
-                    <span
-                      className={classes.projectName}
-                      onClick={() => onQuickstart(project)}
-                    >
-                      {project.name}
-                    </span>
-                    <span className={classes.projectLastOpened}>
-                      last opened on{" "}
-                      {moment(project.updatedAt).format("DD-MM-YYYY")}
-                    </span>
-                    <SimpleButton
-                      className={classes.projectTrashcan}
-                      onClick={() => removeProject(project)}
-                    >
-                      <img src={trash} alt="trash icon" />
-                    </SimpleButton>
-                  </li>
-                ))}
-            </ul>
-          </div>
-        ) : (
-          <div className={classes.bodyCenter}>
-            <div>
-              To start, you need to create a project or <br />
-              <Link to="">open</Link> an existing folder
-            </div>
-          </div>
-        ),
-    },
-    {
-      id: "about",
-      label: "About",
-      content: (
-        <div className={classes.bodyCenter}>
-          <div>
-            Flowser 2022, v1.1 <br />
-            Connect with us on{" "}
-            <a href="https://discord.gg/WJe6CKfp" title="Flowser discord">
-              Discord
-            </a>
-            <br />
-            Follow us on{" "}
-            <a href="https://twitter.com/onflowser" title="Flowser twitter">
-              Twitter
-            </a>{" "}
-            <br />
-            Contribute on{" "}
-            <a
-              href="https://github.com/onflowser/flowser"
-              title="Flowser discord"
-            >
-              Github
-            </a>
-          </div>
-        </div>
-      ),
-    },
-  ];
-
   return (
     <div className={classes.container}>
       <aside className={classes.sidebar}>
@@ -152,21 +83,20 @@ const Main: FunctionComponent<IProps> = (props) => {
           </span>
         </div>
         <ul className={classes.sideBarBody}>
-          {tabs.map((tabData) => (
+          {tabs.map((tab) => (
             <li
-              key={tabData.id}
+              key={tab.id}
               className={classNames(classes.tabWrapper, {
-                [classes.activeTab]:
-                  tab == tabData.id || (tabData.default && !tab),
+                [classes.activeTab]: tab.id === activeTab.id,
               })}
             >
-              <Link to={`/start#${tabData.id}`} className={classes.tabLink}>
+              <Link to={`/start#${tab.id}`} className={classes.tabLink}>
                 <img
                   src={yellowLine}
                   alt="yellow line"
                   className={classes.yellowLine}
                 />
-                {tabData.label}
+                {tab.label}
               </Link>
             </li>
           ))}
@@ -192,11 +122,102 @@ const Main: FunctionComponent<IProps> = (props) => {
           </IconButton>
         </div>
       </aside>
-      {tab
-        ? tabs.find((t) => t.id == tab)?.content
-        : tabs.find((t) => t.default)?.content}
+      {activeTab.content}
     </div>
   );
 };
+
+function ProjectsListContent() {
+  const { projects } = useGetAllProjects()?.data ?? {};
+  const { searchTerm, setPlaceholder } = useSearch("projectSearch");
+  const { removeProject } = useProjectActions();
+  const history = useHistory();
+  const projectService = ProjectsService.getInstance();
+
+  const showProjectList = projects && projects.length > 0;
+
+  useEffect(() => {
+    setPlaceholder("Search projects");
+  }, []);
+
+  const onQuickstart = async (project: Project) => {
+    try {
+      await projectService.useProject(project.id);
+      splitbee.track(`Start: use ${project}`);
+      history.push(`/${routes.firstRouteAfterStart}`);
+    } catch (e: unknown) {
+      console.error("Can't open project:", e);
+      toast.error("Can't open project");
+    }
+  };
+
+  if (!showProjectList) {
+    return (
+      <div className={classes.bodyCenter}>
+        <div>
+          To start, you need to create a project or <br />
+          <Link to="">open</Link> an existing folder
+        </div>
+      </div>
+    );
+  }
+
+  const filteredProjects = projects?.filter(
+    (p) => !searchTerm || p.name.includes(searchTerm)
+  );
+
+  return (
+    <div className={classes.projectList}>
+      <div className={classes.projectListHeader}>
+        <Search className={classes.projectSearch} context="projectSearch" />
+      </div>
+      <ul className={classes.projectListBody}>
+        {filteredProjects.map((project) => (
+          <li key={project.id} className={classes.projectWrapper}>
+            <span
+              className={classes.projectName}
+              onClick={() => onQuickstart(project)}
+            >
+              {project.name}
+            </span>
+            <span className={classes.projectLastOpened}>
+              last opened on {moment(project.updatedAt).format("DD-MM-YYYY")}
+            </span>
+            <SimpleButton
+              className={classes.projectTrashcan}
+              onClick={() => removeProject(project)}
+            >
+              <img src={trash} alt="trash icon" />
+            </SimpleButton>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function AboutContent() {
+  return (
+    <div className={classes.bodyCenter}>
+      <div>
+        Flowser 2022, v1.1 <br />
+        Connect with us on{" "}
+        <a href="https://discord.gg/WJe6CKfp" title="Flowser discord">
+          Discord
+        </a>
+        <br />
+        Follow us on{" "}
+        <a href="https://twitter.com/onflowser" title="Flowser twitter">
+          Twitter
+        </a>{" "}
+        <br />
+        Contribute on{" "}
+        <a href="https://github.com/onflowser/flowser" title="Flowser discord">
+          Github
+        </a>
+      </div>
+    </div>
+  );
+}
 
 export default Main;
