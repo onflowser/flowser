b02d73619c5c221a6bb940a62970b65b9c660152
Bart <bartolomej.kozorog@gmail.com>
2022-09-06 04:20:00 +0200

    ExtendableStorageCard style tweaks

--- frontend/src/pages/accounts/details/ExtendableStorageCard.tsx
+++ frontend/src/pages/accounts/details/ExtendableStorageCard.tsx
@@ -28,22 +28,6 @@ export function ExtendableStorageCard({
     [classes.circleClosed]: expendedCardIds.has(content.pathIdentifier),
   });
 
-  const dumyData = {
-    flowEpochsDKGAdmin: {
-      ResourceType: {
-        Location: {
-          Type: "AddressLocation",
-          Address: "0xf8d6e0586b0a20c7",
-          Name: " FlowDKG",
-        },
-        QualifiedIdentifier: "FlowDKG.Admin",
-        Fields: [{ Identifier: "uuid", Type: {} }],
-        Initializers: null,
-      },
-      Fields: [17],
-    },
-  };
-
   return (
     <div className={extendClass}>
       <div className={classes.header}>
@@ -62,23 +46,17 @@ export function ExtendableStorageCard({
         </div>
       </div>
       <div className={classes.body}>
-        <div className={classes.content}>
-          <div className={classes.title}>{content.pathIdentifier}</div>
-          {expendedCardIds.has(content.pathIdentifier) ? (
-            <div className={classes.json}>
-              <pre className={classes.jsonText}>
-                {JSON.stringify(dumyData, undefined, 2)}
-              </pre>
-            </div>
-          ) : (
-            <div className={classes.tags}>
-              <div className={classes.badge}>ResourceType</div>
-              <div className={classes.badge}>
-                flowContractAuditVouchersAdmin
-              </div>
-            </div>
-          )}
-        </div>
+        <div className={classes.title}>{content.pathIdentifier}</div>
+        {expendedCardIds.has(content.pathIdentifier) ? (
+          <pre className={classes.json}>
+            {JSON.stringify(content.data, undefined, 2)}
+          </pre>
+        ) : (
+          <div className={classes.tags}>
+            <div className={classes.badge}>ResourceType</div>
+            <div className={classes.badge}>flowContractAuditVouchersAdmin</div>
+          </div>
+        )}
       </div>
     </div>
   );
