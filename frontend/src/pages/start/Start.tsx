bc6cbc32a1b4e6ec4db6a5ee6d2f020d01c68e3f
jakob <drusany.jakob@gmail.com>
2022-09-07 23:21:35 +0200

    Add back button to project config

--- frontend/src/pages/start/Start.tsx
+++ frontend/src/pages/start/Start.tsx
@@ -1,14 +1,29 @@
 import React, { FunctionComponent } from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route, RouteProps, Switch } from "react-router-dom";
 import Main from "./main/Main";
 import Configuration from "./configuration/Configuration";
+import Layout from "components/layout/Layout";
+
+const RouteWithLayout = (props: RouteProps) => (
+  <Layout>
+    <Route {...props} />
+  </Layout>
+);
 
 const Start: FunctionComponent = () => {
   return (
     <div>
       <Switch>
         <Route exact path={`/start`} component={Main} />
-        <Route path={`/start/configure/:id?`} component={Configuration} />
+        <Route
+          path={`/start/configure`}
+          exact={true}
+          component={Configuration}
+        />
+        <RouteWithLayout
+          path={`/start/configure/:id?`}
+          component={Configuration}
+        />
         <Redirect from="*" to={`/start`} />
       </Switch>
     </div>
