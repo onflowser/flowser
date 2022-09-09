1a80e409d66cfa9c364d6220e117b289ba336362
Bart <bartolomej.kozorog@gmail.com>
2022-09-09 15:31:24 +0200

    pr fixes

--- frontend/src/pages/start/configuration/Configuration.tsx
+++ frontend/src/pages/start/configuration/Configuration.tsx
@@ -123,206 +123,211 @@ export const Configuration: FunctionComponent = () => {
   return (
     <div className={classes.root}>
       <div className={classes.innerWrapper}>
-        <h1 className={classes.title}>Project settings</h1>
-        <ConfigurationSection title="Flowser" className={classes.section}>
-          <Card className={classes.card}>
-            <TextField
-              label="Project name"
-              description="Give your project a cool name"
-              path="name"
-              formik={formik}
-            />
-            <TextField
-              label="Project root path"
-              description="Specify the filesystem path to your project"
-              path="filesystemPath"
-              formik={formik}
-            />
-          </Card>
-        </ConfigurationSection>
-        <ConfigurationSection
-          title="Dev wallet"
-          className={classes.section}
-          toggleButtonTitle="Run dev wallet"
-          isEnabled={formik.values.devWallet?.run}
-          onToggleEnabled={(isEnabled) =>
-            formik.setFieldValue("devWallet.run", isEnabled)
-          }
-          isToggleable
-        >
-          <Card className={classes.card}>
-            <DevWalletTextField
-              label="Port"
-              path="port"
-              description="Port to start the dev-wallet on"
-              formik={formik}
-            />
-          </Card>
-        </ConfigurationSection>
-        <ConfigurationSection
-          title="Emulator"
-          className={classes.section}
-          toggleButtonTitle="Run flow emulator"
-          isEnabled={formik.values.emulator?.run}
-          onToggleEnabled={(isEnabled) =>
-            formik.setFieldValue("emulator.run", isEnabled)
-          }
-          isToggleable
-        >
-          <Card className={classes.card}>
-            <EmulatorTextField
-              label="RPC Port"
-              path="grpcServerPort"
-              description="gRPC port to listen on"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="HTTP Port"
-              path="restServerPort"
-              description="REST API port to listen on"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Admin Port"
-              path="adminServerPort"
-              description="Admin API port to listen on"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Service Private Key"
-              path="servicePrivateKey"
-              description="Private key used for the service account"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Service Public Key"
-              path="servicePublicKey"
-              description="Public key used for the service account"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Block time"
-              path="blockTime"
-              description="Time between sealed blocks. Valid units are ns, us (or µs), ms, s, m, h"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Database path"
-              path="databasePath"
-              description="Specify path for the database file persisting the state"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Token supply"
-              path="tokenSupply"
-              description="Initial FLOW token supply"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Transaction Expiry"
-              path="transactionExpiry"
-              description="Transaction expiry, measured in blocks"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Transaction Expiry"
-              path="storagePerFlow"
-              description="Specify size of the storage in MB for each FLOW in account balance. Default value from the flow-go"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Minimum Account Balance"
-              path="minAccountBalance"
-              description="Specify minimum balance the account must have. Default value from the flow-go"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Transaction Max Gas Limit"
-              path="transactionMaxGasLimit"
-              description="Maximum gas limit for transactions"
-              formik={formik}
-            />
-            <EmulatorTextField
-              label="Script Gas Limit"
-              path="scriptGasLimit"
-              description="Specify gas limit for script execution"
-              formik={formik}
-            />
-          </Card>
-          <Card className={classNames(classes.card, classes.radioCard)}>
-            <EmulatorRadioField
-              className={classes.radioField}
-              label="Service Signature Algorithm"
-              path="serviceSignatureAlgorithm"
-              formik={formik}
-              options={getSignatureAlgorithmRadioOptions([
-                SignatureAlgorithm.ECDSA_P256,
-                SignatureAlgorithm.ECDSA_secp256k1,
-                SignatureAlgorithm.BLS_BLS12_381,
-              ])}
-            />
-            <EmulatorRadioField
-              className={classes.radioField}
-              label="Service Hash Algorithm"
-              path="serviceHashAlgorithm"
-              formik={formik}
-              options={getHashAlgorithmRadioOptions([
-                HashAlgorithm.SHA2_256,
-                HashAlgorithm.SHA2_384,
-                HashAlgorithm.SHA3_256,
-                HashAlgorithm.SHA3_384,
-                HashAlgorithm.KMAC128_BLS_BLS12_381,
-                HashAlgorithm.KECCAK_256,
-              ])}
-            />
-          </Card>
-          <Card className={classes.card}>
-            <EmulatorToggleField
-              label="Verbose"
-              path="verboseLogging"
-              description="Enable verbose logging"
-              formik={formik}
-            />
-            <EmulatorToggleField
-              label="Persist"
-              path="persist"
-              description="Enable persistence of the state between restarts"
-              formik={formik}
-            />
-            <EmulatorToggleField
-              label="Simple Addresses"
-              path="simpleAddresses"
-              description="Use sequential addresses starting with 0x1"
-              formik={formik}
-            />
-            <EmulatorToggleField
-              label="Storage Limit"
-              path="storageLimit"
-              description="Enable account storage limit"
-              formik={formik}
-            />
-            <EmulatorToggleField
-              label="Transaction Fees"
-              path="transactionFees"
-              description="Enable transaction fees"
-              formik={formik}
-            />
-            <EmulatorToggleField
-              label="Init"
-              path="performInit"
-              description="Generate and set a new service account"
-              formik={formik}
-            />
-            <EmulatorToggleField
-              label="Contracts"
-              path="withContracts"
-              description="Start with contracts like FUSD, NFT and an NFT Marketplace, when the emulator starts"
-              formik={formik}
-            />
-          </Card>
-        </ConfigurationSection>
+        <header>
+          <h1 className={classes.title}>Project settings</h1>
+        </header>
 
-        <div className={classes.footer}>
+        <main>
+          <ConfigurationSection title="Flowser" className={classes.section}>
+            <Card className={classes.card}>
+              <TextField
+                label="Project name"
+                description="Give your project a cool name"
+                path="name"
+                formik={formik}
+              />
+              <TextField
+                label="Project root path"
+                description="Specify the filesystem path to your project"
+                path="filesystemPath"
+                formik={formik}
+              />
+            </Card>
+          </ConfigurationSection>
+          <ConfigurationSection
+            title="Dev wallet"
+            className={classes.section}
+            toggleButtonTitle="Run dev wallet"
+            isEnabled={formik.values.devWallet?.run}
+            onToggleEnabled={(isEnabled) =>
+              formik.setFieldValue("devWallet.run", isEnabled)
+            }
+            isToggleable
+          >
+            <Card className={classes.card}>
+              <DevWalletTextField
+                label="Port"
+                path="port"
+                description="Port to start the dev-wallet on"
+                formik={formik}
+              />
+            </Card>
+          </ConfigurationSection>
+          <ConfigurationSection
+            title="Emulator"
+            className={classes.section}
+            toggleButtonTitle="Run flow emulator"
+            isEnabled={formik.values.emulator?.run}
+            onToggleEnabled={(isEnabled) =>
+              formik.setFieldValue("emulator.run", isEnabled)
+            }
+            isToggleable
+          >
+            <Card className={classes.card}>
+              <EmulatorTextField
+                label="RPC Port"
+                path="grpcServerPort"
+                description="gRPC port to listen on"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="HTTP Port"
+                path="restServerPort"
+                description="REST API port to listen on"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Admin Port"
+                path="adminServerPort"
+                description="Admin API port to listen on"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Service Private Key"
+                path="servicePrivateKey"
+                description="Private key used for the service account"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Service Public Key"
+                path="servicePublicKey"
+                description="Public key used for the service account"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Block time"
+                path="blockTime"
+                description="Time between sealed blocks. Valid units are ns, us (or µs), ms, s, m, h"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Database path"
+                path="databasePath"
+                description="Specify path for the database file persisting the state"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Token supply"
+                path="tokenSupply"
+                description="Initial FLOW token supply"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Transaction Expiry"
+                path="transactionExpiry"
+                description="Transaction expiry, measured in blocks"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Transaction Expiry"
+                path="storagePerFlow"
+                description="Specify size of the storage in MB for each FLOW in account balance. Default value from the flow-go"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Minimum Account Balance"
+                path="minAccountBalance"
+                description="Specify minimum balance the account must have. Default value from the flow-go"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Transaction Max Gas Limit"
+                path="transactionMaxGasLimit"
+                description="Maximum gas limit for transactions"
+                formik={formik}
+              />
+              <EmulatorTextField
+                label="Script Gas Limit"
+                path="scriptGasLimit"
+                description="Specify gas limit for script execution"
+                formik={formik}
+              />
+            </Card>
+            <Card className={classNames(classes.card, classes.radioCard)}>
+              <EmulatorRadioField
+                className={classes.radioField}
+                label="Service Signature Algorithm"
+                path="serviceSignatureAlgorithm"
+                formik={formik}
+                options={getSignatureAlgorithmRadioOptions([
+                  SignatureAlgorithm.ECDSA_P256,
+                  SignatureAlgorithm.ECDSA_secp256k1,
+                  SignatureAlgorithm.BLS_BLS12_381,
+                ])}
+              />
+              <EmulatorRadioField
+                className={classes.radioField}
+                label="Service Hash Algorithm"
+                path="serviceHashAlgorithm"
+                formik={formik}
+                options={getHashAlgorithmRadioOptions([
+                  HashAlgorithm.SHA2_256,
+                  HashAlgorithm.SHA2_384,
+                  HashAlgorithm.SHA3_256,
+                  HashAlgorithm.SHA3_384,
+                  HashAlgorithm.KMAC128_BLS_BLS12_381,
+                  HashAlgorithm.KECCAK_256,
+                ])}
+              />
+            </Card>
+            <Card className={classes.card}>
+              <EmulatorToggleField
+                label="Verbose"
+                path="verboseLogging"
+                description="Enable verbose logging"
+                formik={formik}
+              />
+              <EmulatorToggleField
+                label="Persist"
+                path="persist"
+                description="Enable persistence of the state between restarts"
+                formik={formik}
+              />
+              <EmulatorToggleField
+                label="Simple Addresses"
+                path="simpleAddresses"
+                description="Use sequential addresses starting with 0x1"
+                formik={formik}
+              />
+              <EmulatorToggleField
+                label="Storage Limit"
+                path="storageLimit"
+                description="Enable account storage limit"
+                formik={formik}
+              />
+              <EmulatorToggleField
+                label="Transaction Fees"
+                path="transactionFees"
+                description="Enable transaction fees"
+                formik={formik}
+              />
+              <EmulatorToggleField
+                label="Init"
+                path="performInit"
+                description="Generate and set a new service account"
+                formik={formik}
+              />
+              <EmulatorToggleField
+                label="Contracts"
+                path="withContracts"
+                description="Start with contracts like FUSD, NFT and an NFT Marketplace, when the emulator starts"
+                formik={formik}
+              />
+            </Card>
+          </ConfigurationSection>
+        </main>
+
+        <footer className={classes.footer}>
           <div>
             <Button
               className={classes.footerButton}
@@ -351,7 +356,7 @@ export const Configuration: FunctionComponent = () => {
               Emulator version: {flowCliInfo?.version || "-"}
             </a>
           </div>
-        </div>
+        </footer>
       </div>
     </div>
   );
