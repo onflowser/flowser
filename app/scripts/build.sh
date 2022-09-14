98cfb5885761bbd70e0da8c9d3459c7efb0bb29d
Bart <bartolomej.kozorog@gmail.com>
2022-09-14 14:04:10 +0200

    fix parent directory missing issue

--- app/scripts/build.sh
+++ app/scripts/build.sh
@@ -13,4 +13,8 @@ cp -r ../../frontend/dist ${BUILD_PATH}/electron
 
 PLATFORM_ARG=$1 # valid values: win, mac, linux, all
 
+# The linux build fails if this directory doesn't exist
+# For some reason it's not automatically created
+mkdir ../release/build/@flowser
+
 yarn run "build-${PLATFORM_ARG}"
