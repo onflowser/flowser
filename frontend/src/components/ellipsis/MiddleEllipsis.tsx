edf64677b97aaea4ff8d689860efaf2de383fc70
Bart <bartolomej.kozorog@gmail.com>
2022-10-25 12:18:39 +0200

    fix middle ellipsis calculation

--- frontend/src/components/ellipsis/MiddleEllipsis.tsx
+++ frontend/src/components/ellipsis/MiddleEllipsis.tsx
@@ -17,31 +17,36 @@ const MiddleEllipsis: FunctionComponent<EllipsisProps> = ({
   const [show, setShow] = useState(false);
   const [state, setState] = useState(children);
 
-  useEffect(() => {
-    const resize = () => {
-      setState(children);
-      const offsetWidth = elRef.current ? elRef.current.offsetWidth : 0;
-      const scrollWidth = elRef.current ? elRef.current.scrollWidth : 0;
-      if (offsetWidth < scrollWidth) {
-        const charWidth = Math.ceil(scrollWidth / children.length);
-        const textHalf = Math.ceil(offsetWidth / charWidth / 2);
-        const ellipsisHalf = Math.ceil(ellipsis.length / charWidth / 2);
-        const half = textHalf - ellipsisHalf - 2; // safety 2 :)
-        const ellipsisText =
-          children.substr(0, half) +
-          ellipsis +
-          children.substring(children.length - half);
-        setState(ellipsisText);
-      }
-      setShow(true);
-    };
+  const makeEllipsis = () => {
+    const offsetWidth = elRef.current ? elRef.current.offsetWidth : 0;
+    const scrollWidth = elRef.current ? elRef.current.scrollWidth : 0;
+    if (offsetWidth < scrollWidth) {
+      const charWidth = Math.ceil(scrollWidth / children.length);
+      const textHalf = Math.ceil(offsetWidth / charWidth / 2);
+      const ellipsisHalf = Math.ceil(ellipsis.length / charWidth / 2);
+      const half = textHalf - ellipsisHalf - 2; // safety 2 :)
+      const ellipsisText =
+        children.substr(0, half) +
+        ellipsis +
+        children.substring(children.length - half);
+      setState(ellipsisText);
+    }
+    setShow(true);
+  };
 
+  const resize = () => {
+    setState(children);
+    // Wait for browser to write DOM updates from setState call (on next event loop)
+    setTimeout(() => makeEllipsis(), 0);
+  };
+
+  useEffect(() => {
     resize();
     window.addEventListener("resize", resize);
     return () => {
       window.removeEventListener("resize", resize);
     };
-  }, []);
+  }, [children]);
 
   return (
     <span
