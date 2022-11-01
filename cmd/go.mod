5407789edd15c49964a665456bb67013135e15ce
Bart <bartolomej.kozorog@gmail.com>
2022-11-01 11:00:49 +0100

    add mvp darwin logic

---
+++ cmd/go.mod
@@ -0,0 +1,10 @@
+module github.com/onflowser/flowser
+
+go 1.18
+
+require (
+	github.com/artdarek/go-unzip v1.0.0 // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+)
---
+++ cmd/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/artdarek/go-unzip"
+	"github.com/google/go-github/github"
+)
+
+func IsInstalled() bool {
+	_, err := os.Stat(getInstallDir())
+	return os.IsNotExist(err)
+}
+
+func Install() {
+	assetDownloadPath := downloadLatestReleaseAsset()
+	defer os.Remove(assetDownloadPath)
+	switch runtime.GOOS {
+	case "darwin":
+		uz := unzip.New(assetDownloadPath, "/Applications")
+		uz.Extract()
+	default:
+		panic("Not implemented")
+	}
+}
+
+func downloadLatestReleaseAsset() string {
+	releaseAsset := getLatestReleaseAsset()
+	resp, _ := http.Get(*releaseAsset.BrowserDownloadURL)
+	defer resp.Body.Close()
+
+	out, _ := os.Create(*releaseAsset.Name)
+	defer out.Close()
+
+	_, err := io.Copy(out, resp.Body)
+	if err != nil {
+		panic("Install failed")
+	}
+
+	return out.Name()
+}
+
+func getLatestReleaseAsset() github.ReleaseAsset {
+	client := github.NewClient(nil)
+	release, _, err := client.Repositories.GetLatestRelease(context.Background(), "onflowser", "flowser")
+	if err != nil {
+		log.Fatal(err)
+	}
+	latestVersion := strings.Replace(*release.TagName, "v", "", 1)
+	for _, asset := range release.Assets {
+		if *asset.Name == getAssetName(latestVersion) {
+			return asset
+		}
+	}
+	panic("No asset found")
+}
+
+func getAssetName(version string) string {
+	isArm := runtime.GOARCH == "arm"
+	switch runtime.GOOS {
+	case "darwin":
+		if isArm {
+			return fmt.Sprintf("Flowser-%s-arm64-mac.zip", version)
+		} else {
+			return fmt.Sprintf("Flowser-%s-mac.zip", version)
+		}
+	default:
+		panic("Not implemented")
+	}
+}
+
+func getInstallDir() string {
+	switch runtime.GOOS {
+	case "darwin":
+		return "/Applications/Flowser.app"
+	default:
+		panic("Not implemented")
+	}
+}
+
+func Run(location string) {
+	switch runtime.GOOS {
+	case "darwin":
+		runDarwin(location)
+	default:
+		panic("Not implemented")
+	}
+}
+
+func runDarwin(location string) {
+	cmd := exec.Command("/Applications/Flowser.app/Contents/MacOS/Flowser", fmt.Sprintf("--project-path=%s", location))
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	// TODO: Add proper error handling
+	// TODO: Implement windows logic
+	Install()
+	Run("/Users/bartkozorog/Projects/flowser-test")
+}
