d80924c9ced6fb4d32687aeaac35cd5200159ef5
Bart <bartolomej.kozorog@gmail.com>
2022-11-13 19:45:35 +0100

    disable sentry init on non-production envirnoments

--- frontend/src/targets/electron/services/sentry-renderer.service.ts
+++ frontend/src/targets/electron/services/sentry-renderer.service.ts
@@ -5,12 +5,17 @@ import {
   MonitoringServiceInt,
 } from "../../../services/monitoring.service";
 
+const enableAnalytics = process.env.NODE_ENV === "production";
+
 export class SentryRendererService implements MonitoringServiceInt {
   private fingerprintService: FingerprintService;
   constructor() {
     this.fingerprintService = new FingerprintService();
   }
   async init(): Promise<void> {
+    if (!enableAnalytics) {
+      return;
+    }
     Sentry.init({
       dsn: "https://38a653b591734e0bbe2695a321f554d9@o1430444.ingest.sentry.io/6781494",
     });
