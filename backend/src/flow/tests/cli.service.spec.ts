22c889c71cda92d3d6d96337adcf716540b79910
Bart <bartolomej.kozorog@gmail.com>
2023-06-07 13:02:08 +0200

    add failing test for account generation with todo

--- backend/src/flow/tests/cli.service.spec.ts
+++ backend/src/flow/tests/cli.service.spec.ts
@@ -3,21 +3,37 @@ import { FlowConfigService } from "../services/config.service";
 import { ProcessManagerService } from "../../processes/process-manager.service";
 import { ProjectEntity } from "../../projects/entities/project.entity";
 
-describe("CliService", function () {
-  it("should return the generated key", async function () {
+describe("FlowCliService", function () {
+  let cliService: FlowCliService;
+
+  beforeAll(async () => {
     const configService = new FlowConfigMockService();
     const processManagerService = new ProcessManagerService();
-    const cliService = new FlowCliService(configService, processManagerService);
+    cliService = new FlowCliService(configService, processManagerService);
 
     const mockProject = new ProjectEntity();
     await cliService.onEnterProjectContext(mockProject);
+  });
 
+  it("should return the generated key", async function () {
     const generatedKey = await cliService.generateKey();
 
     expect(generatedKey.private).toBeDefined();
     expect(generatedKey.public).toBeDefined();
     expect(generatedKey.derivationPath).toBeDefined();
     expect(generatedKey.mnemonic).toBeDefined();
+  });
+
+  // TODO(custom-wallet): This test will fail, since it requires a running flow emulator
+  it("should return the generated key", async function () {
+    const generatedKey = await cliService.generateKey();
+    const createdAccount = await cliService.createAccount([generatedKey]);
+
+    expect(createdAccount.address).toBeDefined();
+    expect(createdAccount.balance).toBeDefined();
+    expect(createdAccount.contracts).toHaveLength(0);
+    expect(createdAccount.keys).toHaveLength(1);
+    expect(createdAccount.keys[0]).toEqual(generatedKey.public);
   });
 });
 
