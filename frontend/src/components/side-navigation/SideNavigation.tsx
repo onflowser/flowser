ba296e4ccc1e10f53a7d96444ceb914374e1263b
Bart <bartolomej.kozorog@gmail.com>
2023-07-06 10:14:57 +0200

    add new icons, update sidebar buttons

--- frontend/src/components/side-navigation/SideNavigation.tsx
+++ frontend/src/components/side-navigation/SideNavigation.tsx
@@ -2,28 +2,42 @@ import React, { ReactElement } from "react";
 import classes from "./SideNavigation.module.scss";
 import { routes } from "../../constants/routes";
 import { NavLink } from "react-router-dom";
+import { FlowserIcon } from "components/icons/Icons";
+import { SizedBox } from "../sized-box/SizedBox";
 
 export function SideNavigation(): ReactElement {
   return (
     <div className={classes.root}>
-      <Link to={routes.accounts} />
-      <Link to={routes.blocks} />
-      <Link to={routes.transactions} />
-      <Link to={routes.contracts} />
-      <Link to={routes.events} />
-      <Link to={routes.project} />
+      <FlowserLogo />
+      <SizedBox height={50} />
+      <ProjectLink to={routes.accounts} icon={FlowserIcon.Account} />
+      <ProjectLink to={routes.blocks} icon={FlowserIcon.Block} />
+      <ProjectLink to={routes.transactions} icon={FlowserIcon.Transaction} />
+      <ProjectLink to={routes.contracts} icon={FlowserIcon.Contract} />
+      <ProjectLink to={routes.events} icon={FlowserIcon.Star} />
+      <ProjectLink to={routes.project} icon={FlowserIcon.ArtistPalette} />
     </div>
   );
 }
 
-function Link(props: { to: string }) {
+function FlowserLogo() {
+  const size = 50;
+  return <FlowserIcon.LogoRound width={size} height={size} />;
+}
+
+function ProjectLink(props: {
+  to: string;
+  icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+}) {
+  const Icon = props.icon;
+  const iconSize = 20;
   return (
     <NavLink
       to={props.to}
-      className={classes.inactivePage}
-      activeClassName={classes.activePage}
+      className={classes.inactiveLink}
+      activeClassName={classes.activeLink}
     >
-      TODO
+      <Icon width={iconSize} height={iconSize} />
     </NavLink>
   );
 }
