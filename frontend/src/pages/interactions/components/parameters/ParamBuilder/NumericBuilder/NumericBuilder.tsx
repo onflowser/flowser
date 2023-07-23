5b64c6cacd85a96d009004c7482fd778b406d323
Bart <bartolomej.kozorog@gmail.com>
2023-07-23 12:52:21 +0200

    handle type value initialization

--- frontend/src/pages/interactions/components/parameters/ParamBuilder/NumericBuilder/NumericBuilder.tsx
+++ frontend/src/pages/interactions/components/parameters/ParamBuilder/NumericBuilder/NumericBuilder.tsx
@@ -1,13 +1,26 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { ParameterBuilder } from "../interface";
 import Input from "../../../../../../components/input/Input";
 
 export function NumericBuilder(props: ParameterBuilder): ReactElement {
   const { parameterValue, setParameterValue } = props;
+
+  const isInitialised = typeof parameterValue === "number";
+
+  useEffect(() => {
+    if (!isInitialised) {
+      setParameterValue(0);
+    }
+  }, [isInitialised]);
+
+  if (!isInitialised) {
+    return <></>;
+  }
+
   return (
     <Input
       type="number"
-      value={Number(parameterValue)}
+      value={parameterValue}
       onChange={(e) => setParameterValue(e.target.valueAsNumber)}
     />
   );
--- frontend/src/pages/interactions/components/parameters/ParamBuilder/TextualBuilder/TextualBuilder.tsx
+++ frontend/src/pages/interactions/components/parameters/ParamBuilder/TextualBuilder/TextualBuilder.tsx
@@ -1,13 +1,26 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { ParameterBuilder } from "../interface";
 import Input from "../../../../../../components/input/Input";
 
 export function TextualBuilder(props: ParameterBuilder): ReactElement {
   const { parameterValue, setParameterValue } = props;
+
+  const isInitialised = typeof parameterValue === "string";
+
+  useEffect(() => {
+    if (!isInitialised) {
+      setParameterValue("");
+    }
+  }, [isInitialised]);
+
+  if (!isInitialised) {
+    return <></>;
+  }
+
   return (
     <Input
       type="text"
-      value={String(parameterValue ?? "")}
+      value={parameterValue}
       onChange={(e) => setParameterValue(e.target.value)}
     />
   );
