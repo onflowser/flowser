8510f7633078dbb2a4ceeaa24266b3ac30556167
Bart <bartolomej.kozorog@gmail.com>
2023-07-24 11:36:38 +0200

    fix dictionary initialization, improve duplicate entires handling

--- frontend/src/pages/interactions/components/parameters/ParamBuilder/DictionaryBuilder/DictionaryBuilder.tsx
+++ frontend/src/pages/interactions/components/parameters/ParamBuilder/DictionaryBuilder/DictionaryBuilder.tsx
@@ -1,11 +1,20 @@
-import React, { ReactElement, useEffect } from "react";
+import React, { ReactElement, useEffect, useRef } from "react";
 import { CadenceValueBuilder } from "../interface";
 import { ParamBuilder } from "../ParamBuilder";
 import { FclDictionaryEntry, FclValue, FclValues } from "@flowser/shared";
+import { MultiMap } from "utils/multi-map";
+import toast from "react-hot-toast";
 
 export function DictionaryBuilder(props: CadenceValueBuilder): ReactElement {
   const { type, value, setValue } = props;
   const { dictionary } = type;
+
+  const defaultEntry: FclDictionaryEntry = {
+    key: undefined,
+    value: undefined,
+  };
+  // A lookup from old keys to new keys
+  const keyRenamingLookup = useRef(new Map<FclValue, FclValue>());
 
   if (!dictionary) {
     throw new Error("Expected dictionary field");
@@ -15,41 +24,81 @@ export function DictionaryBuilder(props: CadenceValueBuilder): ReactElement {
 
   useEffect(() => {
     if (!isInitialized) {
-      setValue([
-        {
-          key: undefined,
-          value: undefined,
-        },
-      ]);
+      setValue([defaultEntry]);
     }
   }, [isInitialized]);
+
+  // Check for duplicated entries by keys,
+  // and remove the last duplicated entry.
+  useEffect(() => {
+    if (!isInitialized) {
+      return;
+    }
+    const valuesByKey = new MultiMap(
+      value.map((entry) => [entry.key, entry.value])
+    );
+    const duplicatedEntry = Array.from(valuesByKey.entries())
+      .filter((entry) => entry[1].length > 1)
+      .map((entry) => entry)[0];
+    if (duplicatedEntry) {
+      const lastDuplicatedEntryIndex = value
+        .reverse()
+        .findIndex((entry) => entry.key == duplicatedEntry[0]);
+      const deduplicatedEntries = [...value];
+      deduplicatedEntries.splice(lastDuplicatedEntryIndex, 1);
+      setValue(deduplicatedEntries);
+      toast.error("Duplicated keys found");
+    }
+  }, [value]);
 
   if (!isInitialized) {
     return <></>;
   }
 
+  // Returns true if target entry was found by the provided key.
   function updateEntry(
     key: FclValue,
     partialEntry: Partial<FclDictionaryEntry>
-  ) {
-    if (isInitialized) {
-      const entryIndex = value.findIndex(
-        (entry: FclDictionaryEntry) => entry.key === key
-      );
+  ): boolean {
+    if (!isInitialized) {
+      throw new Error("Not initialized");
+    }
+    const entryIndex = value.findIndex(
+      (entry: FclDictionaryEntry) => entry.key === key
+    );
+    const isEntryFound = entryIndex !== -1;
+    if (isEntryFound) {
       value[entryIndex] = { ...value[entryIndex], ...partialEntry };
-      setValue(value);
+      setValue([...value]);
+    }
+    return isEntryFound;
+  }
+
+  function updateEntryKey(oldKey: FclValue, newKey: FclValue) {
+    updateEntry(oldKey, { key: newKey });
+    keyRenamingLookup.current.set(oldKey, newKey);
+  }
+
+  function updateEntryValue(key: FclValue, value: FclValue) {
+    const isKeyFound = updateEntry(key, { value });
+    if (!isKeyFound) {
+      // The following scenario happens during initialization phase:
+      // 1. entry key is updated
+      // 2. entry value is updated using the old key immediately after
+      // This makes the second update unsuccessful, as the outdated key is used.
+      // To solve this, without needing to change the child components,
+      // we store a lookup of the key renaming and use the new key when update fails.
+      const newKey = keyRenamingLookup.current.get(key);
+      const isNewKeyFound = updateEntry(newKey, { value });
+      if (!isNewKeyFound) {
+        throw new Error("Expecting to found entry with new key");
+      }
     }
   }
 
   function addEntry() {
     if (isInitialized) {
-      setValue([
-        ...value,
-        {
-          key: undefined,
-          value: undefined,
-        },
-      ]);
+      setValue([...value, defaultEntry]);
     }
   }
 
@@ -69,7 +118,7 @@ export function DictionaryBuilder(props: CadenceValueBuilder): ReactElement {
               <ParamBuilder
                 type={dictionary.key}
                 value={entry.key}
-                setValue={(newKey) => updateEntry(entry.key, { key: newKey })}
+                setValue={(newKey) => updateEntryKey(entry.key, newKey)}
               />
             </div>
             <div>
@@ -77,9 +126,7 @@ export function DictionaryBuilder(props: CadenceValueBuilder): ReactElement {
               <ParamBuilder
                 type={dictionary.value}
                 value={entry.value}
-                setValue={(newValue) =>
-                  updateEntry(entry.key, { value: newValue })
-                }
+                setValue={(newValue) => updateEntryValue(entry.key, newValue)}
               />
             </div>
           </div>
