8ed6dd2f010eb59666a96b30b2c22e226ab2b670
Bart <bartolomej.kozorog@gmail.com>
2023-08-20 15:10:40 +0200

    fix transaction name calculation

--- frontend/src/pages/interactions/hooks/use-interaction-name.ts
+++ frontend/src/pages/interactions/hooks/use-interaction-name.ts
@@ -12,7 +12,22 @@ export function useInteractionName(
 
   return useMemo(
     () =>
-      templates.find((template) => template.code === props.sourceCode)?.name,
+      templates.find(
+        (template) =>
+          props.sourceCode &&
+          template.code &&
+          // Ignore imports for comparison,
+          // since those can differ due to address replacement.
+          // See: https://developers.flow.com/tooling/fcl-js/api#address-replacement
+          stripImports(template.code) === stripImports(props.sourceCode)
+      )?.name,
     [props.sourceCode, templates]
   );
+}
+
+function stripImports(code: string) {
+  return code
+    .split("\n")
+    .filter((line) => !line.startsWith("import"))
+    .join("\n");
 }
