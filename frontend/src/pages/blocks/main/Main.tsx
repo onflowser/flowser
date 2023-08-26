0ea9107602429f1e998b9637de8901b601e68b95
Bart <bartolomej.kozorog@gmail.com>
2023-08-26 14:26:06 +0200

    remove snapshot action from blocks table

--- frontend/src/pages/blocks/main/Main.tsx
+++ frontend/src/pages/blocks/main/Main.tsx
@@ -8,31 +8,15 @@ import { useNavigation } from "../../../hooks/use-navigation";
 import { createColumnHelper } from "@tanstack/table-core";
 import Table from "../../../components/table/Table";
 import { Block } from "@flowser/shared";
-import {
-  useGetPollingBlocks,
-  useGetPollingEmulatorSnapshots,
-} from "../../../hooks/use-api";
-import { SimpleButton } from "../../../components/buttons/simple-button/SimpleButton";
-import { ReactComponent as SnapshotIcon } from "../../../assets/icons/snapshot.svg";
+import { useGetPollingBlocks } from "../../../hooks/use-api";
 import ReactTimeago from "react-timeago";
-import { useProjectActions } from "../../../contexts/project.context";
 import { DecoratedPollingEntity } from "contexts/timeout-polling.context";
 
 const columnHelper = createColumnHelper<DecoratedPollingEntity<Block>>();
 
 const Main: FunctionComponent = () => {
   const { showNavigationDrawer } = useNavigation();
-  const { checkoutBlock } = useProjectActions();
-
   const { data: blocks, firstFetch, error } = useGetPollingBlocks();
-  const { data: emulatorSnapshots } = useGetPollingEmulatorSnapshots();
-  const snapshotLookupByBlockId = useMemo(
-    () =>
-      new Map(
-        emulatorSnapshots.map((snapshot) => [snapshot.blockId, snapshot])
-      ),
-    [emulatorSnapshots]
-  );
 
   useEffect(() => {
     showNavigationDrawer(false);
@@ -73,34 +57,8 @@ const Main: FunctionComponent = () => {
           </Value>
         ),
       }),
-      columnHelper.display({
-        id: "snapshot",
-        header: () => <Label variant="medium">SNAPSHOT</Label>,
-        meta: {
-          className: classes.snapshot,
-        },
-        cell: (info) => {
-          const block = info.row.original;
-          const latestBlock = blocks[0];
-          const isLatestBlock = latestBlock.height === block.height;
-          const snapshot = snapshotLookupByBlockId.get(block.id);
-          return (
-            <Value>
-              {!isLatestBlock && (
-                <SimpleButton
-                  style={{ marginRight: 10 }}
-                  onClick={() => checkoutBlock(block.id)}
-                >
-                  <SnapshotIcon />
-                </SimpleButton>
-              )}
-              {snapshot?.description}
-            </Value>
-          );
-        },
-      }),
     ],
-    [snapshotLookupByBlockId, blocks, checkoutBlock]
+    [blocks]
   );
 
   return (
