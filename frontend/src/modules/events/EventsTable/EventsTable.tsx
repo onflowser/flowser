7726eaae201e4f04df043219685769e2d446608e
Bart <bartolomej.kozorog@gmail.com>
2023-09-05 11:47:51 +0200

    show event data inline, simplify events table

--- frontend/src/modules/events/EventsTable/EventsTable.tsx
+++ frontend/src/modules/events/EventsTable/EventsTable.tsx
@@ -1,54 +1,72 @@
-import React, { useState, useMemo, ReactElement } from "react";
+import React, { ReactElement } from "react";
 import classes from "./EventsTable.module.scss";
-import tableClasses from "../../../components/table/Table.module.scss";
-import Card from "../../../components/card/Card";
 import Label from "../../../components/label/Label";
 import Value from "../../../components/value/Value";
-import { NavLink } from "react-router-dom";
 import MiddleEllipsis from "../../../components/ellipsis/MiddleEllipsis";
-import CaretIcon from "../../../components/caret-icon/CaretIcon";
 import { createColumnHelper } from "@tanstack/table-core";
 import { Event } from "@flowser/shared";
-import { ComputedEventData, EventUtils } from "../../../utils/event-utils";
-import CopyButton from "../../../components/buttons/copy-button/CopyButton";
 import Table from "../../../components/table/Table";
-import { flexRender } from "@tanstack/react-table";
 import ReactTimeago from "react-timeago";
-import classNames from "classnames";
 import { DecoratedPollingEntity } from "contexts/timeout-polling.context";
 import { Ellipsis } from "../../../components/ellipsis/Ellipsis";
 import { ProjectLink } from "../../../components/links/ProjectLink";
+import { JsonView } from "../../../components/json-view/JsonView";
 
-const subTableColumnHelper = createColumnHelper<ComputedEventData>();
-const subTableColumns = [
-  subTableColumnHelper.accessor("name", {
-    header: () => <Label variant="medium">ARGUMENT NAME</Label>,
+const columnHelper = createColumnHelper<DecoratedPollingEntity<Event>>();
+
+const columns = [
+  columnHelper.accessor("blockId", {
+    header: () => <Label variant="medium">BLOCK</Label>,
     cell: (info) => (
       <Value>
-        <MiddleEllipsis className={classes.subTableValue}>
-          {info.getValue()}
-        </MiddleEllipsis>
+        <ProjectLink to={`/blocks/${info.getValue()}`}>
+          <MiddleEllipsis className={classes.hashEvents}>
+            {info.getValue()}
+          </MiddleEllipsis>
+        </ProjectLink>
       </Value>
     ),
   }),
-  subTableColumnHelper.accessor("type", {
-    header: () => <Label variant="medium">ARGUMENT TYPE</Label>,
+  columnHelper.accessor("transactionId", {
+    header: () => <Label variant="medium">TRANSACTION</Label>,
+    cell: (info) => (
+      <Value>
+        <ProjectLink to={`/transactions/${info.getValue()}`}>
+          <MiddleEllipsis className={classes.hashEvents}>
+            {info.getValue()}
+          </MiddleEllipsis>
+        </ProjectLink>
+      </Value>
+    ),
+  }),
+  columnHelper.accessor("type", {
+    header: () => <Label variant="medium">TYPE</Label>,
+    meta: {
+      className: classes.typeColumn,
+    },
     cell: (info) => (
       <Value style={{ width: "100%" }}>
-        <MiddleEllipsis className={classes.subTableValue}>
-          {info.getValue()}
-        </MiddleEllipsis>
+        <Ellipsis elementName="pre">{info.getValue()}</Ellipsis>
       </Value>
     ),
   }),
-  subTableColumnHelper.accessor("value", {
-    header: () => <Label variant="medium">ARGUMENT VALUE</Label>,
+  columnHelper.accessor("createdAt", {
+    header: () => <Label variant="medium">CREATED</Label>,
     cell: (info) => (
       <Value>
-        <Ellipsis elementName="pre" className={classes.subTableValue}>
-          {info.getValue()}
-        </Ellipsis>
-        <CopyButton value={info.getValue()} />
+        <ReactTimeago date={info.getValue()} />
+      </Value>
+    ),
+  }),
+  columnHelper.accessor("data", {
+    header: () => <Label variant="medium">DATA</Label>,
+    cell: (info) => (
+      <Value>
+        <JsonView
+          name="data"
+          collapseAtDepth={1}
+          data={info.getValue() as Record<string, unknown>}
+        />
       </Value>
     ),
   }),
@@ -59,126 +77,10 @@ type EventsTableProps = {
 };
 
 export function EventsTable(props: EventsTableProps): ReactElement {
-  const [openedLog, setOpenedLog] = useState("");
-  const columnHelper = createColumnHelper<DecoratedPollingEntity<Event>>();
-
-  const columns = useMemo(
-    () => [
-      columnHelper.accessor("blockId", {
-        header: () => <Label variant="medium">BLOCK</Label>,
-        cell: (info) => (
-          <Value>
-            <ProjectLink to={`/blocks/${info.getValue()}`}>
-              <MiddleEllipsis className={classes.hashEvents}>
-                {info.getValue()}
-              </MiddleEllipsis>
-            </ProjectLink>
-          </Value>
-        ),
-      }),
-      columnHelper.accessor("transactionId", {
-        header: () => <Label variant="medium">TRANSACTION</Label>,
-        cell: (info) => (
-          <Value>
-            <ProjectLink to={`/transactions/${info.getValue()}`}>
-              <MiddleEllipsis className={classes.hashEvents}>
-                {info.getValue()}
-              </MiddleEllipsis>
-            </ProjectLink>
-          </Value>
-        ),
-      }),
-      columnHelper.accessor("type", {
-        header: () => <Label variant="medium">TYPE</Label>,
-        meta: {
-          className: classes.typeColumn,
-        },
-        cell: (info) => (
-          <Value style={{ width: "100%" }}>
-            <Ellipsis elementName="pre">{info.getValue()}</Ellipsis>
-          </Value>
-        ),
-      }),
-      columnHelper.accessor("createdAt", {
-        header: () => <Label variant="medium">CREATED</Label>,
-        cell: (info) => (
-          <Value>
-            <ReactTimeago date={info.getValue()} />
-          </Value>
-        ),
-      }),
-      columnHelper.display({
-        id: "caret",
-        meta: {
-          className: classes.caretColumn,
-        },
-        cell: ({ row }) => (
-          <CaretIcon
-            inverted={true}
-            className={classes.icon}
-            isOpen={openedLog === row.id}
-            onChange={(status) => openLog(status, row.id)}
-          />
-        ),
-      }),
-    ],
-    [openedLog]
-  );
-
-  const openLog = (status: boolean, id: string) => {
-    setOpenedLog(!status ? id : "");
-  };
-
   return (
     <Table<DecoratedPollingEntity<Event>>
       data={props.events}
       columns={columns}
-      renderCustomHeader={(headerGroup) => (
-        <Card
-          className={classNames(
-            tableClasses.tableRow,
-            classes.tableRow,
-            tableClasses.headerRow
-          )}
-          key={headerGroup.id}
-        >
-          {headerGroup.headers.map((header) => (
-            <div
-              key={header.id}
-              className={header.column.columnDef.meta?.className}
-            >
-              {flexRender(header.column.columnDef.header, header.getContext())}
-            </div>
-          ))}
-        </Card>
-      )}
-      renderCustomRow={(row) => (
-        <React.Fragment key={row.original.id}>
-          <Card
-            className={classNames(tableClasses.tableRow, classes.tableRow)}
-            showIntroAnimation={row.original.isNew}
-          >
-            {row.getVisibleCells().map((cell) => (
-              <div
-                key={cell.id}
-                className={cell.column.columnDef.meta?.className}
-              >
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </div>
-            ))}
-          </Card>
-          {openedLog === row.id && row.original && (
-            <div>
-              <Table<ComputedEventData>
-                data={EventUtils.computeEventData(row.original.data)}
-                columns={subTableColumns}
-                bodyRowClass={classes.subTableRow}
-                headerRowClass={classes.subTableRow}
-              />
-            </div>
-          )}
-        </React.Fragment>
-      )}
     />
   );
 }
