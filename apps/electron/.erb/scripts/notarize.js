e4ed12c8681e7181ee81f163977ecc111a479f9b
Bart <bartolomej.kozorog@gmail.com>
2023-11-02 10:33:07 +0100

    fix app ID

--- apps/electron/.erb/scripts/notarize.js
+++ apps/electron/.erb/scripts/notarize.js
@@ -1,5 +1,4 @@
 const { notarize } = require('@electron/notarize');
-const { build } = require('../../package.json');
 
 exports.default = async function notarizeMacos(context) {
   const { electronPlatformName, appOutDir } = context;
@@ -16,7 +15,7 @@ exports.default = async function notarizeMacos(context) {
     !('APPLE_ID' in process.env && 'APPLE_APP_SPECIFIC_PASSWORD' in process.env)
   ) {
     console.warn(
-      'Skipping notarizing step. APPLE_ID and APPLE_APP_SPECIFIC_PASSWORD env variables must be set',
+      'Skipping notarizing step. APPLE_ID and APPLE_APP_SPECIFIC_PASSWORD env variables must be set'
     );
     return;
   }
@@ -24,7 +23,8 @@ exports.default = async function notarizeMacos(context) {
   const appName = context.packager.appInfo.productFilename;
 
   await notarize({
-    appBundleId: build.appId,
+    // Must be kept in sync with `appId` field in `electron-builder.yml`
+    appBundleId: 'dev.flowser.app',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
