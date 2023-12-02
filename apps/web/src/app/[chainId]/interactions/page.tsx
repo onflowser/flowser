dc1bf3b12a89c54ad897e32cb1580abd2c31ee1b
Bart <bartolomej.kozorog@gmail.com>
2023-12-02 20:48:00 +0100

    call parse interaction

--- apps/web/src/app/[chainId]/interactions/page.tsx
+++ apps/web/src/app/[chainId]/interactions/page.tsx
@@ -1,32 +1,64 @@
-"use client"
+"use client";
 import { InteractionsPage } from "@onflowser/ui/src/interactions/InteractionsPage";
-import { WorkspaceLayout } from "@onflowser/ui/src/common/layouts/ProjectLayout/WorkspaceLayout";
 import { InteractionRegistryProvider } from "@onflowser/ui/src/interactions/contexts/interaction-registry.context";
 import { TemplatesRegistryProvider } from "@onflowser/ui/src/interactions/contexts/templates.context";
 import { NavigationProvider } from "@onflowser/ui/src/contexts/navigation.context";
 import { ReactNode } from "react";
 import { useParams, useSelectedLayoutSegments, usePathname, useSearchParams, useRouter } from "next/navigation";
-import { ServiceRegistryProvider } from "@onflowser/ui/src/contexts/service-registry.context";
+import {
+  ExecuteScriptRequest,
+  IInteractionService, SendTransactionRequest,
+  ServiceRegistryProvider
+} from "@onflowser/ui/src/contexts/service-registry.context";
+import { InteractionTemplate, ParsedInteractionOrError } from "@onflowser/api";
 
-export default function Page() {
-  return(
-    <NextJsNavigationProvider>
-      <ServiceRegistryProvider services={{} as never}>
-          <InteractionRegistryProvider>
-            <TemplatesRegistryProvider>
-              <InteractionsPage />
-            </TemplatesRegistryProvider>
-          </InteractionRegistryProvider>
-      </ServiceRegistryProvider>
-    </NextJsNavigationProvider>
-  )
+
+class InteractionsService implements IInteractionService {
+  executeScript(request: ExecuteScriptRequest): Promise<any> {
+    return Promise.resolve(undefined);
+  }
+
+  getTemplates(): Promise<InteractionTemplate[]> {
+    return Promise.resolve([]);
+  }
+
+  async parse(sourceCode: string): Promise<ParsedInteractionOrError> {
+    const url = new URL("http://localhost:3000/flow-emulator/interactions/parsed");
+    url.searchParams.append("sourceCode", sourceCode);
+    return fetch(url).then(res => res.json())
+  }
+
+  sendTransaction(request: SendTransactionRequest): Promise<{ transactionId: string }> {
+    return Promise.resolve({ transactionId: "" });
+  }
+
 }
 
-function NextJsNavigationProvider(props: {children: ReactNode}) {
+
+export default function Page() {
+  return (
+    <NextJsNavigationProvider>
+      <ServiceRegistryProvider
+        // @ts-ignore
+        services={{
+          interactionsService: new InteractionsService()
+        }}
+      >
+        <InteractionRegistryProvider>
+          <TemplatesRegistryProvider>
+            <InteractionsPage />
+          </TemplatesRegistryProvider>
+        </InteractionRegistryProvider>
+      </ServiceRegistryProvider>
+    </NextJsNavigationProvider>
+  );
+}
+
+function NextJsNavigationProvider(props: { children: ReactNode }) {
   const params = useParams();
   const matches = useSelectedLayoutSegments();
   const pathname = usePathname();
-  const { } = useRouter();
+  const {} = useRouter();
   const search = useSearchParams();
 
   function navigate() {
@@ -34,7 +66,7 @@ function NextJsNavigationProvider(props: {children: ReactNode}) {
   }
 
 
-  console.log({ matches, pathname })
+  console.log({ matches, pathname });
   return (
     <NavigationProvider controller={{
       params,
@@ -50,5 +82,5 @@ function NextJsNavigationProvider(props: {children: ReactNode}) {
     }}>
       {props.children}
     </NavigationProvider>
-  )
+  );
 }
