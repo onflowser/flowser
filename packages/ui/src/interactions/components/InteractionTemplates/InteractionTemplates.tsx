dc6d1bee520fec0c18df4b8c17ea59285d9fa0ae
Bart <bartolomej.kozorog@gmail.com>
2023-12-18 09:44:20 +0100

    move flix info to separate component (mainly to avoid conflicting header class names)

--- packages/ui/src/interactions/components/InteractionTemplates/InteractionTemplates.tsx
+++ packages/ui/src/interactions/components/InteractionTemplates/InteractionTemplates.tsx
@@ -15,6 +15,7 @@ import { FLOW_FLIX_URL, useFlixSearch } from "../../../hooks/flix";
 import { ExternalLink } from "../../../common/links/ExternalLink/ExternalLink";
 import { LineSeparator } from "../../../common/misc/LineSeparator/LineSeparator";
 import { Shimmer } from "../../../common/loaders/Shimmer/Shimmer";
+import { FlixInfo } from "../FlixInfo/FlixInfo";
 
 export function InteractionTemplates(): ReactElement {
   return (
@@ -157,61 +158,6 @@ function WorkspaceTemplateInfo(props: { workspaceTemplate: WorkspaceTemplate }) 
         <IdeLink.VsCode filePath={workspaceTemplate.filePath} />
         <IdeLink.WebStorm filePath={workspaceTemplate.filePath} />
         <IdeLink.IntellijIdea filePath={workspaceTemplate.filePath} />
-      </div>
-    </div>
-  );
-}
-
-function FlixInfo(props: { sourceCode: string }) {
-  const { data, isLoading } = useFlixSearch({
-    sourceCode: props.sourceCode,
-    network: "any"
-  });
-
-  if (isLoading) {
-    return <Shimmer height={150} />;
-  }
-
-  const isVerified = data !== undefined;
-
-  return (
-    <div className={classes.flixInfo}>
-      <div className={classes.header}>
-        <ExternalLink className={classes.title} inline href="https://developers.flow.com/build/advanced-concepts/flix">
-          FLIX:
-        </ExternalLink>
-        {isVerified ? (
-          <Fragment>
-            verified
-            <FlowserIcon.VerifiedCheck className={classes.verifiedIcon} />
-          </Fragment>
-        ) : (
-          <Fragment>
-            unverified
-            <FlowserIcon.CircleCross className={classes.unverifiedIcon} />
-          </Fragment>
-        )}
-      </div>
-      <LineSeparator horizontal />
-      <div className={classes.body}>
-        {isVerified ? (
-          <Fragment>
-            <p>{data.data.messages.description?.i18n["en-US"]}</p>
-            <ExternalLink inline href={`${FLOW_FLIX_URL}/v1/templates/${data.id}`} />
-          </Fragment>
-        ) : (
-          <Fragment>
-            <p>
-              This interaction is not yet verified by FLIX.
-            </p>
-            <ExternalLink
-              inline
-              href="https://github.com/onflow/flow-interaction-template-service#-propose-interaction-template"
-            >
-              Submit for verification
-            </ExternalLink>
-          </Fragment>
-        )}
       </div>
     </div>
   );
