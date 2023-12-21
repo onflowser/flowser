54dbdfb6d46d50a537f14ee94953a96622292438
Bart <bartolomej.kozorog@gmail.com>
2023-12-21 10:54:06 +0100

    migrate to react-modal

--- packages/ui/src/common/overlays/dialogs/base/BaseDialog.tsx
+++ packages/ui/src/common/overlays/dialogs/base/BaseDialog.tsx
@@ -4,43 +4,39 @@ import React, {
   ReactNode
 } from "react";
 import classes from "./BaseDialog.module.scss";
-import classNames from "classnames";
-import { BaseCard } from "../../../cards/BaseCard/BaseCard";
 import { FlowserIcon } from "../../../icons/FlowserIcon";
+import ReactModal from 'react-modal';
 
 export type DialogProps = {
   children: ReactNode;
   onClose: MouseEventHandler<HTMLDivElement>;
   className?: string;
+  zIndex?: string;
 };
 
 export const BaseDialog: FunctionComponent<DialogProps> = ({
-  children,
   onClose,
-  className,
+  zIndex = 10,
+  children,
+  className
 }) => {
-  const onOutsideClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    event.stopPropagation();
-    onClose(event);
-  };
-
-  const onClickInside = (event: React.MouseEvent<HTMLDivElement>) => {
-    event.stopPropagation();
-  };
 
   return (
-    <div className={classes.root} onClick={onOutsideClick}>
-      <div className={classNames(classes.dialog, className)}>
-        <BaseCard className={classes.card} onClick={onClickInside}>
-          <FlowserIcon.Close
-            className={classes.closeButton}
-            width={20}
-            height={20}
-            onClick={onClose}
-          />
-          {children}
-        </BaseCard>
-      </div>
-    </div>
+    <ReactModal
+      className={classes.content}
+      overlayClassName={classes.overlay}
+      isOpen={true}
+      onRequestClose={onClose}
+      shouldCloseOnEsc={true}
+      shouldCloseOnOverlayClick={true}
+    >
+      <FlowserIcon.Close
+        className={classes.closeButton}
+        width={20}
+        height={20}
+        onClick={onClose}
+      />
+      {children}
+    </ReactModal>
   );
 };
