d4d729d9e97196da907a3c143f79d5602001e04b
Bart <bartolomej.kozorog@gmail.com>
2024-01-22 09:36:48 +0100

    add install go script

---
+++ packages/nodejs/install-go.js
@@ -0,0 +1,55 @@
+const { exec } = require('child_process/promises');
+const https = require('https');
+const fs = require('fs');
+const os = require('os');
+
+async function isGoInstalled() {
+  try {
+    const { stdout } = await exec('go version');
+    console.log('Go is already installed:', stdout.trim());
+    return true;
+  } catch (error) {
+    console.log('Go is not installed');
+    return false;
+  }
+}
+
+async function downloadFile(url, dest) {
+  const file = fs.createWriteStream(dest);
+  https.get(url, (response) => {
+    response.pipe(file);
+    file.on('finish', () => {
+      file.close();
+      console.log('Downloaded Go');
+    });
+  });
+}
+
+function installGo(filePath) {
+  exec(`tar -C /usr/local -xzf ${filePath}`, async (error) => {
+    if (error) {
+      console.error('Error occurred:', error);
+      return;
+    }
+    console.log('Go installed successfully');
+    // Updating PATH to include Go (this will only affect this script's process)
+    process.env.PATH += ':/usr/local/go/bin';
+    const { stdout } = await exec('go version');
+    console.log('Installed Go version:', stdout.trim());
+  });
+}
+
+async function main() {
+  const goInstalled = await isGoInstalled();
+  console.log("Platform", os.platform())
+  if (!goInstalled && os.platform() === "linux") {
+    const goVersion = '1.19'; // Update this as needed
+    const platform = 'linux-amd64';
+    const url = `https://golang.org/dl/go${goVersion}.${platform}.tar.gz`;
+    const dest = `/tmp/go${goVersion}.${platform}.tar.gz`;
+    await downloadFile(url, dest);
+    installGo(dest);
+  }
+}
+
+main();
