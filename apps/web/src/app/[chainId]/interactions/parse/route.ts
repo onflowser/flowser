bcd1d6f879ecbef2f73ec644d74fe65a7d59f2c5
Bart <bartolomej.kozorog@gmail.com>
2024-01-22 14:15:24 +0100

    Fix Next.js parse endpoint (#218)
    * refactor to post endpoint
    
    * add todo
    
    * fix error response
    
    * add more error handling
    
    * attempt to fix 405 error
    
    * add @onflowser/nodejs to web package.json
    
    * attempt to install go via npm
    
    * Revert "attempt to install go via npm"
    
    This reverts commit 071de7670f0abe528e8f43e5739a4a3ba5462e69.
    
    * add install go script
    
    * fix exec usage
    
    * fix async usage
    
    * fix install url
    
    * add go to path, fix rm command
    
    * add logging
    
    * fix logging
    
    * attempt to include bin in final output
    
    * remove tree command
    
    * fix setup-bin
    
    * fix bin path
    
    * attempt to fix bin path
    
    * update build script
    
    * add debug logs
    
    * add debug logs
    
    * add test find command
    
    * add workaround for retrieving project root
    
    * add debug calls
    
    * final fix
    
    * tweaks

--- apps/web/src/app/[chainId]/interactions/parse/route.ts
+++ apps/web/src/app/[chainId]/interactions/parse/route.ts
@@ -1,27 +1,46 @@
+import {GoBindingsService} from "@onflowser/nodejs"
 import path from "path";
-import {GoBindingsService} from "../../../../../../../packages/nodejs/src"
+
+const isDev = process.env.NODE_ENV === "development";
 
 const goBindingsService = new GoBindingsService({
-  binDirPath: path.join(
-    __dirname,
-    '../../../../../../../..',
-    'packages',
-    'nodejs',
-    'bin',
-  ),
+  // In dev we can pull the bin directly from the `nodejs/bin` dir in the monorepo.
+  // While in prod we must use the one that's copied to the current folder with `setup-bin` script.
+  binDirPath: isDev
+    ? path.join(
+        __dirname,
+        '../../../../../../../..',
+        'packages',
+        'nodejs',
+        'bin',
+      )
+    // https://github.com/vercel/next.js/issues/8251#issuecomment-657770901
+    : path.resolve("./public/bin"),
 });
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const sourceCode = searchParams.get('sourceCode');
+export async function POST(request: Request) {
+  const requestBody = await request.json();
+  const sourceCode = requestBody?.sourceCode;
 
-  if (sourceCode === null) {
-    throw new Error("Missing `sourceCode` query param")
+  if (!sourceCode) {
+    return Response.json({
+      message: "Missing `sourceCode` field in body"
+    }, {
+      status: 400
+    })
   }
 
-  const parsedInteraction = await goBindingsService.getParsedInteraction({
-    sourceCode
-  })
+  try {
+    const parsedInteraction = await goBindingsService.getParsedInteraction({
+      sourceCode
+    });
 
-  return Response.json(parsedInteraction)
+    return Response.json(parsedInteraction)
+  } catch (error: any) {
+    return Response.json({
+      message: `Error parsing: ${error.message}`
+    }, {
+      status: 500
+    })
+  }
 }
