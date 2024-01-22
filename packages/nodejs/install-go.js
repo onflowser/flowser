95de37a016681a3840a1d7c49363e148d51d8d87
Bart <bartolomej.kozorog@gmail.com>
2024-01-22 09:43:44 +0100

    fix async usage

--- packages/nodejs/install-go.js
+++ packages/nodejs/install-go.js
@@ -19,39 +19,46 @@ async function isGoInstalled() {
 
 async function downloadFile(url, dest) {
   const file = fs.createWriteStream(dest);
-  https.get(url, (response) => {
-    response.pipe(file);
-    file.on('finish', () => {
-      file.close();
-      console.log('Downloaded Go');
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close();
+        resolve(file);
+      });
+      file.on("error", (error) => {
+        reject(error);
+      })
     });
   });
 }
 
 function installGo(filePath) {
-  exec(`tar -C /usr/local -xzf ${filePath}`, async (error) => {
-    if (error) {
-      console.error('Error occurred:', error);
-      return;
-    }
-    console.log('Go installed successfully');
-    // Updating PATH to include Go (this will only affect this script's process)
-    process.env.PATH += ':/usr/local/go/bin';
-    const { stdout } = await execAsync('go version');
-    console.log('Installed Go version:', stdout.trim());
-  });
+  return new Promise((resolve, reject) => {
+    exec(`tar -C /usr/local -xzf ${filePath}`, async (error) => {
+      if (error) {
+        console.error('Error occurred:', error);
+        return reject(error);
+      }
+      console.log('Go installed successfully');
+      // Updating PATH to include Go (this will only affect this script's process)
+      process.env.PATH += ':/usr/local/go/bin';
+      const { stdout } = await execAsync('go version');
+      console.log('Installed Go version:', stdout.trim());
+      resolve();
+    });
+  })
 }
 
 async function main() {
   const goInstalled = await isGoInstalled();
-  console.log("Platform", os.platform())
   if (!goInstalled && os.platform() === "linux") {
     const goVersion = '1.19'; // Update this as needed
     const platform = 'linux-amd64';
     const url = `https://golang.org/dl/go${goVersion}.${platform}.tar.gz`;
     const dest = `/tmp/go${goVersion}.${platform}.tar.gz`;
     await downloadFile(url, dest);
-    installGo(dest);
+    await installGo(dest);
   }
 }
 
